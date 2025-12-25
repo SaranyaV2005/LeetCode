0306093533606ac66cba7b2c94eca0799b9eb616
SaranyaV2005 <sara1905nya@gmail.com>
2025-12-25 20:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (27.63%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        int left=1;
+        int right=num/2;
+        while(left<=right){
+            int mid=(left+right)/2;
+            long sq=(long)mid*mid;
+            if(sq==num) return true;
+            else if(sq<num) left=mid+1;
+            else right=mid-1;
+
+        }return false;
+    }
+}
